6d7f78041ddb5182f73fa411598a89c6fa67291d
Amandha W. Barok <amandha.barok@gmail.com>
2021-05-24 11:58:59 -0700

    Primeiros passos com React

---
+++ bloco_11/firstStepsReact/hello-world-react/src/Component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Amandha Wingert Barok</h1>
+        <p>Estudante de Desenvolvimento Web.</p>
+      </div>
+    )
+  }
+}
+export default Component;
---
+++ bloco_11/firstStepsReact/testando-meu-computador/testando.jsx
@@ -0,0 +1,3 @@
+const textJSX = 'Hello, JSX'
+const classe = 'hello-class';
+const element = <h1 className={classe}>{textJSX}</h1>
