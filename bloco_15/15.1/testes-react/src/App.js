2dd90d66f9ae1a366ab05bba76249f7c32bf4adf
Amandha W. Barok <amandha.barok@gmail.com>
2021-06-22 06:25:44 -0700

    Fixation exercises

---
+++ bloco_15/15.1/testes-react/src/App.js
@@ -0,0 +1,52 @@
+    // App.js
+    import React from 'react';
+    import './App.css';
+    import ValidEmail from './ValidEmail';
+    
+    class App extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          email: '',
+          saveEmail: '',
+        };
+      }
+
+      changeEmail(value) {
+        this.setState({ email: value });
+      }
+
+      changeSaveEmail(value) {
+        this.setState({ saveEmail: value, email: ''});
+      }
+      render () {
+        const { email, saveEmail } = this.state;
+        return (
+          <div className="App">
+            <label htmlFor="id-email">
+              Email
+            </label>
+            <input 
+              id="id-email"
+              value={email}
+              type="email"
+              onChange={(event) => this.changeEmail(event.target.value)}
+            />
+
+            <input 
+              id="btn-send"
+              type="button"
+              data-testid="id-send"
+              value="Enviar" 
+              onClick={() => this.changeSaveEmail(email)}
+            />
+
+            <input id="btn-back" type="button" value="Voltar" />
+
+            <ValidEmail email={saveEmail} />
+          </div>
+        );
+      }
+      }
+    
+    export default App;
